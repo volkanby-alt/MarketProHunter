8fe950ce37c4c20f9329ffd1931fd0c41c02a4d4
volkanby-alt <volkanby@gmail.com>
2026-07-02 22:45:54 +0300

    Add search settings model

---
+++ src/MarketProHunter/Models/SearchSettings.cs
@@ -0,0 +1,15 @@
+namespace MarketProHunter.Models;
+
+public sealed record SearchSettings
+{
+    public string MarketplaceBaseUrl { get; init; } = "https://www.amazon.com";
+    public string ZipCode { get; init; } = "07073";
+    public decimal MinPrice { get; init; } = 9m;
+    public decimal MaxPrice { get; init; } = 98m;
+    public bool RequireAmazonChoice { get; init; } = true;
+    public bool ExcludeLowStock { get; init; } = true;
+    public bool ExcludeUsuallyKeepItem { get; init; } = true;
+    public int DelayBetweenPagesMs { get; init; } = 2500;
+
+    public static SearchSettings Default => new();
+}
